a812cb95fba3764dcdfdbfbf3218ea0f66c6d83c
liyong <hungrybirder@gmail.com>
2022-04-26 15:03:46 +0800

    Add HelloTest.java

--- src/main/java/io/hungrybirder/Hello.java
+++ src/main/java/io/hungrybirder/Hello.java
@@ -2,7 +2,7 @@ package io.hungrybirder;
 
 public class Hello {
 
-  public static void main(String[] args) {
-      System.out.println("hello world");
+  public static String cool() {
+    return "cool";
   }
 }
---
+++ src/test/java/io/hungrybirder/HelloTest.java
@@ -0,0 +1,13 @@
+package io.hungrybirder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloTest {
+
+  @Test
+  public void testCool() {
+    Assert.assertEquals("cool", Hello.cool());
+    Assert.assertEquals("cool", Hello.cool());
+  }
+}

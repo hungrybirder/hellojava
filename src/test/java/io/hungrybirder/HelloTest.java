3453cc9b2db9b9ebe7f36efdce61a6cde91660f5
liyong <hungrybirder@gmail.com>
2023-09-27 19:42:52 +0800

    Update HelloTest

--- src/test/java/io/hungrybirder/HelloTest.java
+++ src/test/java/io/hungrybirder/HelloTest.java
@@ -1,12 +1,13 @@
 package io.hungrybirder;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class HelloTest {
 
     @Test
     public void testCool() {
-        Assert.assertEquals("cool", Hello.cool());
+        assertEquals("cool", Hello.cool());
     }
 }
